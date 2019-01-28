59513c30ca27b73ea931d97dd8bb3af2398aeb24
recofka <recofka@gmail.com>
2019-01-28 18:09:05 +0100

    set action

--- src/components/GetBreeds.js
+++ src/components/GetBreeds.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import * as request from 'superagent'
 import { connect } from 'react-redux';
 
+
 class GetBreeds extends React.Component {
   state = {}
 
@@ -9,11 +10,12 @@ class GetBreeds extends React.Component {
     request('https://dog.ceo/api/breeds/list/all')
       .then(response => this.setState({ breeds: response.body }))
 
-      // setBreed(this.props.setBreed())
+      // setBreeds(this.props.setBreeds({denise2: 'denise2'}))
+      // addAlbums(this.props.addAlbums(9998,'Enjoying moonlight'));
   }
 
   render() {
-    //console.log('this.props',this.props)
+    
     console.log('this.state',this.state)
     return <div>
         <p> {this.props.state}</p>
