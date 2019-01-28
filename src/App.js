3c612b80d175083dacac85eb5795cde831abad0c
quietQuiet <j.lietunovas@gmail.com>
2019-01-28 16:44:40 +0100

    created a container for dog data, created image container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import DogNameComponent from './components/DogNameComponent'
+import ImageComponent from './components/ImageComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Dog App</h1>
-          <button>First Breed</button>
-          <button>Second Breed</button>
-          <button>Third Breed</button>
+          <ImageComponent></ImageComponent>
+          <h1>Who's this Pokémon?</h1>
+          <div className="name-container">
+            <DogNameComponent></DogNameComponent>
+            <DogNameComponent></DogNameComponent>
+            <DogNameComponent></DogNameComponent>
+          </div>
+          
         </header>
       </div>
     );
---
+++ src/components/ImageComponent.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import './ImageComponent.css';
+
+export default function ImageComponent() {
+
+        return (
+            <img className='pic' src="http://www.magicdogs.it/wp-content/uploads/2013/12/diamante_venturina.jpg"></img>
+        )
+}
