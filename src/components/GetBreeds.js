7b16e81276eab7697858b6fb617d52507ab40e91
recofka <recofka@gmail.com>
2019-01-28 17:57:14 +0100

    start read pros from store

--- src/components/GetBreeds.js
+++ src/components/GetBreeds.js
@@ -1,19 +1,31 @@
 import * as React from 'react'
 import * as request from 'superagent'
+import { connect } from 'react-redux';
 
-
-export default class GetBreeds extends React.Component {
+class GetBreeds extends React.Component {
   state = {}
 
   componentDidMount() {
     request('https://dog.ceo/api/breeds/list/all')
-      .then(response => this.setState({ state: response.body }))
+      .then(response => this.setState({ breeds: response.body }))
+
+      // setBreed(this.props.setBreed())
   }
 
   render() {
-    console.log(this.state)
+    //console.log('this.props',this.props)
+    console.log('this.state',this.state)
     return <div>
-        <p>test</p>
+        <p> {this.props.state}</p>
     </div>
   }
 }
+
+const mapStateToProps = (state) => {
+  //console.log('state',state)
+ return  {
+      breeds : state.breeds
+ }
+}
+
+export default connect(mapStateToProps)(GetBreeds)
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,3 +1,5 @@
-export default (state = [], action = {}) => {
+const inicial = {denise: 'denise'}
+
+export default (state = inicial, action = {}) => {
     return state
   }
