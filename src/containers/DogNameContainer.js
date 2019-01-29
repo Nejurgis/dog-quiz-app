abf6fc8d64749bb99f1d9d780dfcd5a03db04e95
quietQuiet <j.lietunovas@gmail.com>
2019-01-29 14:36:50 +0100

    random doggos

--- src/containers/DogNameContainer.js
+++ src/containers/DogNameContainer.js
@@ -3,12 +3,26 @@ import DogNameComponent from '../components/DogNameComponent'
 
 
 export default class DogNameContainer extends React.Component {
+
+    
+
+
     render() {
+        // console.log(this.props.value)
+        const randomDogNumber1 = Math.floor(Math.random() * Object.keys(this.props.value).length)
+        const randomDogNumber2 = Math.floor(Math.random() * Object.keys(this.props.value).length)
+        const randomDogNumber3 = Math.floor(Math.random() * Object.keys(this.props.value).length)
+
+        const randomDog1 = Object.keys(this.props.value)[randomDogNumber1]
+        const randomDog2 = Object.keys(this.props.value)[randomDogNumber2]
+        const randomDog3 = Object.keys(this.props.value)[randomDogNumber3]
+        
+
         return (
             <div>
-                <DogNameComponent submitAnswer={this.submitAnswer} name={this.props}></DogNameComponent>
-                <DogNameComponent submitAnswer={this.submitAnswer}name={this.props}></DogNameComponent>
-                <DogNameComponent submitAnswer={this.submitAnswer}name={this.props}></DogNameComponent>
+                <DogNameComponent submitAnswer={this.submitAnswer} name={randomDog1}></DogNameComponent>
+                <DogNameComponent submitAnswer={this.submitAnswer} name={randomDog2}></DogNameComponent>
+                <DogNameComponent submitAnswer={this.submitAnswer} name={randomDog3}></DogNameComponent>
             </div>
         )
     }
