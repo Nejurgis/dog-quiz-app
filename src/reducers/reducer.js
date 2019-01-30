2c7959b54b1d0a2d429335b85227ef390c31ae94
quietQuiet <j.lietunovas@gmail.com>
2019-01-30 13:24:45 +0100

    made re-render work

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -2,7 +2,7 @@ const reducer = (state = [], action = {}) => {
     switch (action.type) {
     case 'SET_IMAGE':
       return [
-        ...state,
+        state,
         action.payload
       ]
   
