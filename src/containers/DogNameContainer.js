c46f45190e6b79d10d1ae1c08154a36fbbe47648
quietQuiet <j.lietunovas@gmail.com>
2019-01-30 13:54:09 +0100

    cleaned syntax, added re-render

--- src/containers/DogNameContainer.js
+++ src/containers/DogNameContainer.js
@@ -5,20 +5,17 @@ import {addIncorrect} from '../actions/addIncorrect'
 import {addCorrect} from '../actions/addCorrect'
 import request from 'superagent'
 import {setImage} from '../actions/setImage'
-// import { instanceOf } from 'prop-types';
-
 
 class DogNameContainer extends React.Component {
     render() {
         const url = this.props.imageUrl.map(item => item.breeds)
-        let n = String(url)
-        const correctName = n.split('/')[4]
-
+        const correctName = String(url).split('/')[4]
+        
         const randomDog = () => {
             let randomNum = Math.floor(Math.random() * Object.keys(this.props.value).length)
             return Object.keys(this.props.value)[randomNum]
         }
-
+        
         const dogArray = [randomDog(), randomDog(), correctName].sort()
 
         return (
@@ -29,25 +26,19 @@ class DogNameContainer extends React.Component {
         )
     }
      
-
     submitAnswer(correctName, dog) {
+        const reRenderComponent = () => {
+            request('https://dog.ceo/api/breeds/image/random')
+            .then(response => this.props.setImage(response.body.message))
+        }
         if (correctName === dog){
-            this.props.addCorrect(dog)
-            const reRenderComppnent = () => {
-                request('https://dog.ceo/api/breeds/image/random')
-                .then(response => this.props.setImage(response.body.message))
-
-            }
-            reRenderComppnent()
-            // call action that will rerender the component
-            // the action will:
-            // 1. get new image//
-            // 2. get new buttons//
-
             alert("Correct!")
-        }else {
+            this.props.addCorrect(dog)
+            reRenderComponent()
+        }else {  
             alert("Wrong")
             this.props.addIncorrect(dog)
+            reRenderComponent()
         }
     }
 }
