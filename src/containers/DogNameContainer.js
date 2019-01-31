41c1dfeaa38e8eee7b66c693128ac2b2da0faf67
recofka <recofka@gmail.com>
2019-01-31 17:02:48 +0100

    Show the correct anwser

--- src/containers/DogNameContainer.js
+++ src/containers/DogNameContainer.js
@@ -13,53 +13,47 @@ class DogNameContainer extends React.Component {
         showAnswer: ''
     }
 
-
-    displayAnswer = (answer) => {
+    displayAnswer = (answer, correctName) => {
         if (answer === 'Right') {
             this.setState({showAnswer: 'Correct!'})
-        } else {
-            this.setState({showAnswer: 'Incorrect!'})
+        }else {
+            this.setState({showAnswer: `Incorrect!! Correct is  ${correctName}`})
         }
-        // setTimeout(this.hideMessage(), 2000)
-
-    }
+      }
   
 
     render() {
-
         const url = this.props.imageUrl.map(item => item.breeds)
         const correctName = String(url).split('/')[4]
         const randomDog = () => {
-            let randomNum = Math.floor(Math.random() * Object.keys(this.props.value).length)
-            return Object.keys(this.props.value)[randomNum]
+        let randomNum = Math.floor(Math.random() * Object.keys(this.props.value).length)
+        return Object.keys(this.props.value)[randomNum]
         }
         const renderButtons = () => {
-            return (
-                <div>
-                    {dogArray.map((dog, index) =>
-                        <DogNameComponent key={index} submitAnswer={() =>
-                            this.submitAnswer(correctName, dog)} name={dog} correct={dog === correctName} />)}
-                </div>
-            )
+            return(
+            <div>
+                {dogArray.map((dog, index) =>
+                <DogNameComponent 
+                    key={index} submitAnswer={() => this.submitAnswer(correctName, dog)} 
+                    name={dog} 
+                    correct={dog === correctName} />)}
+            </div>)
         }
+        
         const dogArray = [randomDog(), randomDog(), correctName].sort()
 
         if (this.props.correctAnswersNum >= 10) {
             alert('you guessed correctly 10 times!')
             dogArray.push(randomDog(), randomDog(), randomDog())
-
         }
         
 
-        return (
-            <div>
-
-                {this.state.showAnswer && <AnswerStatus props={this.state.showAnswer}/>}
-                 {renderButtons()}
-            
-            </div>
-        )
-    }
+        return (<div>
+                    {this.state.showAnswer && <AnswerStatus props={this.state.showAnswer} className='feedback'/>}
+                    {renderButtons()}
+                </div>)
+                
+        }
 
     submitAnswer(correctName, dog) {
         const reRenderComponent = () => {
@@ -72,12 +66,14 @@ class DogNameContainer extends React.Component {
         }
        
         if (correctName === dog){
-            this.displayAnswer('Right')
+            this.displayAnswer('Right', correctName)
             this.props.addCorrect(dog)
             setTimeout(reRenderComponent, 2000)
             setTimeout(hideMessage, 2000)
+            
+            
         } else {  
-            this.displayAnswer('Wrong')
+            this.displayAnswer('Wrong', correctName)
             this.props.addIncorrect(dog)
             setTimeout(reRenderComponent, 2000)
             setTimeout(hideMessage, 2000)
