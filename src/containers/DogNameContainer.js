1eaca092f1d1f3cc9827023051d7848895655760
quietQuiet <j.lietunovas@gmail.com>
2019-01-31 10:16:39 +0100

    added what"s the correct button

--- src/containers/DogNameContainer.js
+++ src/containers/DogNameContainer.js
@@ -5,13 +5,28 @@ import {addIncorrect} from '../actions/addIncorrect'
 import {addCorrect} from '../actions/addCorrect'
 import request from 'superagent'
 import {setImage} from '../actions/setImage'
+import App from '../App';
 
 class DogNameContainer extends React.Component {
     render() {
 
         const url = this.props.imageUrl.map(item => item.breeds)
         const correctName = String(url).split('/')[4]
-        
+        const clicky = (event) => {
+            this.setState({selectedItem: event.currentTarget.dataset.id})
+            console.log('works?')
+        }
+
+        function findReactElement(node) {
+            for (var key in node) {
+                if (key.startsWith("__reactInternalInstance$")) {
+                    return node[key]._debugOwner.stateNode;
+                }
+            }
+            return null;
+        }
+        // console.log(findReactElement(DogNameComponent))
+
         const randomDog = () => {
             let randomNum = Math.floor(Math.random() * Object.keys(this.props.value).length)
             return Object.keys(this.props.value)[randomNum]
@@ -21,10 +36,12 @@ class DogNameContainer extends React.Component {
             return (
                 <div>
                     {dogArray.map((dog, index) => 
-                    <DogNameComponent key={index} submitAnswer={() => this.submitAnswer(correctName, dog)} name={dog} />)}
+                    <DogNameComponent key={index} submitAnswer={() => this.submitAnswer(correctName, dog)} name={dog} correct={dog === correctName} />)}
                 </div>
             )
         }
+
+        console.log(this.props)
         
         const dogArray = [randomDog(), randomDog(), correctName].sort()
         
@@ -36,6 +53,7 @@ class DogNameContainer extends React.Component {
     
         return (
             renderButtons()
+            
         )
     }
      
@@ -44,8 +62,12 @@ class DogNameContainer extends React.Component {
             request('https://dog.ceo/api/breeds/image/random')
             .then(response => this.props.setImage(response.body.message))
         }
+       
         if (correctName === dog){
             // alert("Correct!")
+            //
+            console.log(this)
+            this.style={color:'green'}
             this.props.addCorrect(dog)
             setTimeout(reRenderComponent, 2000)
         } else {  
@@ -57,11 +79,9 @@ class DogNameContainer extends React.Component {
 
 }
 const mapStateToProps = (state) => {
-
     return {
         imageUrl: state.breeds,
         correctAnswersNum: state.answerReducer.correct.length
-
     }
 }
 
